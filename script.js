162950f79cd002b0fc96c32af462b996e450e534
Fay L <flocean682@gmail.com>
2020-06-13 00:58:36 +0000

    random picture

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ function clearBox(elementID)
 {
     document.getElementById(elementID).innerHTML = " ";
 }
+function newPic(){
+     var img = document.getElementById('img'); 
+            img.src = 'https://picsum.photos/300'; 
+}
+
 function getJoke(){
   clearBox('output');
+      newPic();
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -13,11 +19,11 @@ xhttp.onreadystatechange = function() {
         var data = JSON.parse(response);
     console.log(data.attachments[0].fallback);
      document.getElementById('output').innerHTML = "<br><p>"+data.attachments[0].fallback+"</p>";
+ 
         }
 };
-
-
 xhttp.open("GET", "https://icanhazdadjoke.com/slack", true);
 
 xhttp.send();
+
 }
